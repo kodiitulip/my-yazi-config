43bd380d927b6c52fa1543196271b7cc573e5af3
Kodie <kodii.tulip@gmail.com>
2025-04-19 00:29:40 -0300

    init commit

---
+++ init.lua
@@ -0,0 +1,6 @@
+require("full-border"):setup({
+	-- Available values: ui.Border.PLAIN, ui.Border.ROUNDED
+	type = ui.Border.ROUNDED,
+})
+
+require("omp"):setup({ config = "/home/kodie/.config/ohmyposh/tokyonight_storm.omp.json" })
